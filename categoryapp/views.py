fdefbe49dbe673f101b0076076567cdf72d6c6d5
alinurdinf <alinurdiansyah919@outlook.co.id>
2024-10-09 14:59:50 +0700

    Intial Commit

---
+++ categoryapp/views.py
@@ -0,0 +1,18 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Category
+from .serializers import CategorySerializer
+
+@api_view(['GET', 'POST'])
+def category_list(request):
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
